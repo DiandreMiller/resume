a78847f1057dfa632e7a78f714dc0171f9c58368
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-05 01:51:52 -0400

    readMe: Readme updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 // import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import AboutMe from '../Components/AboutMe';
-import ContactMe from '../Components/ContactMe';
-import Education from '../Components/Education';
-import Experience from '../Components/Experience';
-import Instructions from '../Components/Instructions';
-import Projects from '../Components/Projects';
-import SoftSkills from '../Components/SoftSkills';
-import Volunteer from '../Components/Volunteer';
+import AboutMe from './Components/AboutMe';
+import ContactMe from './Components/ContactMe';
+import Education from './Components/Education';
+import Experience from './Components/Experience';
+import Hobbies from "./Components/Hobbies";
+import Instructions from './Components/Instructions';
+import Projects from './Components/Projects';
+import Scoreboard from './Components/Scoreboard';
+import SoftSkills from './Components/SoftSkills';
+import Volunteer from './Components/Volunteer';
+
 
 
 
@@ -18,15 +21,17 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-5xl font-bold underline">Hello world!</h1>
-        <AboutMe />
-        <ContactMe />
-        <Education />
-        <Experience />
-        <Instructions />
-        <Projects />
-        <SoftSkills />
-        <Volunteer />
+        <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Hello world!</h1>
+        {/* <AboutMe /> */}
+        {/* <ContactMe /> */}
+        {/* <Education /> */}
+        {/* <Experience /> */}
+        {/* <Instructions /> */}
+        {/* <Projects /> */}
+        <Scoreboard />
+        <Hobbies />
+        {/* <SoftSkills /> */}
+        {/* <Volunteer /> */}
       </div>
       
     </>
---
+++ src/Components/Hobbies.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hobbies = () => {
+
+    return (
+        <div>
+            <p className="font-serif">Playing Basketball</p>
+            <p className="font-mono">Watching Basketball (Go Lakers!)</p>
+            <p className="font-sans">Listening to Music</p>
+            <p>Working Out</p>
+        </div>
+    )
+
+}
+
+export default Hobbies;
