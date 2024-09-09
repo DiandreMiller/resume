2a6379bcea58bcafa5c09d358ccfe8159a93875c
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-09 02:01:08 -0400

    feature: added god job after clicking see my resume

--- src/Pages/FourOFour.tsx
+++ src/Pages/FourOFour.tsx
@@ -1,7 +1,7 @@
 //Dependencies 
 import React from "react"
 import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import { Link, ScrollRestoration, useNavigate } from "react-router-dom"
 
 //Images
 import shocked from '../Assets/shocked.gif'
@@ -9,6 +9,7 @@ import walkingAway from '../Assets/walkingAway.GIF'
 import doIt from '../Assets/doIt.GIF'
 import goodJob from '../Assets/goodJob.GIF'
 import bye from '../Assets/BYE.GIF'
+import Home from "./Home"
 
 const FourOFour = () => {
 
@@ -17,6 +18,10 @@ const FourOFour = () => {
     const [visibleDoIt, setVisibleDoIt] = useState<boolean>(false);
     const [visibleGoodJob, setVisibleGoodJob] = useState<boolean>(false);
     const [visibleBye, setVisibleBye] = useState<boolean>(false);
+    const [nonExistingPage, setNonExistingPage] = useState<boolean>(true);
+    const [visibleResume, setVisibleResume] = useState<boolean>(false);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const timer: number = setTimeout(() => {
@@ -27,25 +32,41 @@ const FourOFour = () => {
         const secondTimer: number = setTimeout(() => {
             setVisibleWalkingAway(false);
             setVisibleDoIt(true);
+            setNonExistingPage(false);
+            setVisibleResume(true);
         }, 6000);
 
-        const thirdTimer: number = setTimeout(() => {
-            setVisibleDoIt(false);
-        }, 11000);
+        // const thirdTimer: number = setTimeout(() => {
+        //     setVisibleDoIt(false);
+        // }, 11000);
 
 
         return () => {
             clearTimeout(timer);
             clearTimeout(secondTimer);
-            clearTimeout(thirdTimer);
+            // clearTimeout(thirdTimer);
         };
         
     }, []);
 
+    const handleResumeClick = (event: React.MouseEvent) => {
+        event.preventDefault();
+        setVisibleDoIt(false);
+        setVisibleGoodJob(true);
+
+        
+        setTimeout(() => {
+            navigate('/')
+        }, 1500);
+    }
+
 
     return (
         <div>
-            <h1 className=""> The page you are looking for does not exist!</h1>
+            {nonExistingPage && (
+                <h1 className=""> The page you are looking for does not exist!</h1>
+            )}
+
             {visibleShocker && (
                 <img src={shocked} alt="A person shocked" />
             )}
@@ -57,6 +78,15 @@ const FourOFour = () => {
             {visibleDoIt && (
                 <img src={doIt} alt="Man saying Do it!"/>
             )}
+
+            {visibleResume && (
+                <h1>Click here to see <Link to='/'onClick={handleResumeClick} >My Resume</Link> </h1>
+            )}
+
+            {visibleGoodJob && (
+                <img src={goodJob} alt="A man with a medal that says good job" />
+            )}
+            
         </div>
     )
 
