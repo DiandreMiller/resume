34f2c79c008ba75418e56e10580b4751a9de76d2
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-21 16:28:36 -0400

    design: added basketball to court

--- src/Components/BasketballCourt.tsx
+++ src/Components/BasketballCourt.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import court from '../Assets/cartoon-basketball-court-vector.jpg'
+import BallBounce from "./BallBounce";
 
 const BasketballCourt = () => {
 
     return (
         <div className="w-screen h-screen bg-cover bg-center transform -translate-x-28 p-0 overflow-x-hidden" style={{ backgroundImage: `url(${court})`}}>
+            <BallBounce />
             
         </div>
 
