20e0ad77eba7498abfed5f7a30072ce3e10c41a2
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-22 15:31:56 -0400

    design: added drifting cloud

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import gitHub from '../Assets/gitHubLogo.png'
 import linkedIn from '../Assets/linkedInLogo.png'
+import hamburgerMenu from '../Assets/hamburgerMenu.png'
 
 import { Link } from "react-router-dom";
 
@@ -12,8 +13,9 @@ const Navbar = () => {
     return (
         <div>
             <nav className="w-full h-16 fixed top-0 left-0 z-10 flex items center" style={{ background: 'rgb(245,136,115)', transform: 'translateY(0px)' }} >
+                <img className="hover:shadow-[0_0_20px_5px_rgba(0,245,136,115)] transition duration-300 ml-2" src={hamburgerMenu} alt="options menu" />
                 
-                <Link to='/'><h2 className="lg:text-4xl start ml-32 rochester" style={{ transform: 'translateY(9px)'}}>Diandre Miller</h2></Link>
+                <Link to='/'><h2 className="lg:text-4xl start ml-32 rochester hover:shadow-[0_0_20px_5px_rgba(0,245,136,115)] transition duration-300" style={{ transform: 'translateY(9px)'}}>Diandre Miller</h2></Link>
                 <div className="ml-auto flex items-center space-x-4 mr-10">
                 <Link to='https://www.linkedin.com/in/diandre-miller/' target="blank"><img className="h-11 hover:shadow-[0_0_20px_5px_rgba(0,245,136,115)] transition duration-300" style={{ transform: 'translateX(-128px)'}} src={linkedIn} /></Link>
                 <Link to='https://github.com/DiandreMiller' target="blank"><img className="h-10 hover:shadow-[0_0_20px_5px_rgba(0,245,136,115)] transition duration-300" style={{ transform: 'translateX(-128px)'}} src={gitHub} /></Link>
