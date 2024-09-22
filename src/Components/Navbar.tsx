69bf4406067b010f07de43af864c7ac1dab278df
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-22 11:20:09 -0400

    functionality: added clickable header to navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -12,7 +12,8 @@ const Navbar = () => {
     return (
         <div>
             <nav className="w-full h-16 fixed top-0 left-0 z-10 flex" style={{ background: 'rgb(245,136,115)', transform: 'translateY(0px)' }} >
-                <h2 className="lg:text-4xl start ml-32 flex items-center rochester">Diandre Miller</h2>
+                
+                <Link to='/'><h2 className="lg:text-4xl start ml-32 flex items-center rochester">Diandre Miller</h2></Link>
                 <div className="ml-auto flex items-center space-x-4 mr-10">
                 <Link to='https://www.linkedin.com/in/diandre-miller/' target="blank"><img className="h-11" style={{ transform: 'translateX(-128px)'}} src={linkedIn} /></Link>
                 <Link to='https://github.com/DiandreMiller' target="blank"><img className="h-10" style={{ transform: 'translateX(-128px)'}} src={gitHub} /></Link>
