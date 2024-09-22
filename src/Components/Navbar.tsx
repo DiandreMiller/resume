21b7ac718ddcb5b23cf3f404aaf9a171e20410f4
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-22 11:43:09 -0400

    design: fixed misaligned navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -11,9 +11,9 @@ const Navbar = () => {
 
     return (
         <div>
-            <nav className="w-full h-16 fixed top-0 left-0 z-10 flex" style={{ background: 'rgb(245,136,115)', transform: 'translateY(0px)' }} >
+            <nav className="w-full h-16 fixed top-0 left-0 z-10 flex items center" style={{ background: 'rgb(245,136,115)', transform: 'translateY(0px)' }} >
                 
-                <Link to='/'><h2 className="lg:text-4xl start ml-32 flex items-center rochester">Diandre Miller</h2></Link>
+                <Link to='/'><h2 className="lg:text-4xl start ml-32 rochester" style={{ transform: 'translateY(9px)'}}>Diandre Miller</h2></Link>
                 <div className="ml-auto flex items-center space-x-4 mr-10">
                 <Link to='https://www.linkedin.com/in/diandre-miller/' target="blank"><img className="h-11 hover:shadow-[0_0_20px_5px_rgba(0,245,136,115)] transition duration-300" style={{ transform: 'translateX(-128px)'}} src={linkedIn} /></Link>
                 <Link to='https://github.com/DiandreMiller' target="blank"><img className="h-10 hover:shadow-[0_0_20px_5px_rgba(0,245,136,115)] transition duration-300" style={{ transform: 'translateX(-128px)'}} src={gitHub} /></Link>
