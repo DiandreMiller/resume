e92c89b4a8c3bca54ab0c62569cf7a013c2ec3a9
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-22 02:45:54 -0400

    design: updated navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,7 +5,9 @@ const Navbar = () => {
 
     return (
         <div>
-            <nav className="w-full h-16 fixed top-0 left-0 z-10" style={{ background: 'rgb(245,136,115)', transform: 'translateY(0px)' }} ></nav>
+            <nav className="w-full h-16 fixed top-0 left-0 z-10 flex" style={{ background: 'rgb(245,136,115)', transform: 'translateY(0px)' }} >
+                <h2 className="lg:text-4xl start ml-32 flex items-center">Diandre Miller</h2>
+            </nav>
         </div>
     )
 }
