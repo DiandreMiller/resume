65fc4810824a95b6af2dd9d3e06b2616d502b276
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-22 03:22:49 -0400

    design: updated navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import gitHub from '../Assets/gitHubLogo.png'
+import linkedIn from '../Assets/linkedInLogo.png'
+
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
@@ -7,6 +11,11 @@ const Navbar = () => {
         <div>
             <nav className="w-full h-16 fixed top-0 left-0 z-10 flex" style={{ background: 'rgb(245,136,115)', transform: 'translateY(0px)' }} >
                 <h2 className="lg:text-4xl start ml-32 flex items-center">Diandre Miller</h2>
+                <div className="ml-auto flex items-center space-x-4 mr-10">
+                <Link to='https://www.linkedin.com/in/diandre-miller/' target="blank"><img className="h-11" style={{ transform: 'translateX(-128px)'}} src={linkedIn} /></Link>
+                <Link to='https://github.com/DiandreMiller' target="blank"><img className="h-10" style={{ transform: 'translateX(-128px)'}} src={gitHub} /></Link>
+                </div>
+                
             </nav>
         </div>
     )
