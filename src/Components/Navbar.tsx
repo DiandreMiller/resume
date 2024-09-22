7fa66308a1b738c3e14b70ce67bfefcb461393e2
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-22 03:27:33 -0400

    design: updated navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -6,11 +6,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
+    //To Do: Glow dark blue when hover on social media links
+
 
     return (
         <div>
             <nav className="w-full h-16 fixed top-0 left-0 z-10 flex" style={{ background: 'rgb(245,136,115)', transform: 'translateY(0px)' }} >
-                <h2 className="lg:text-4xl start ml-32 flex items-center">Diandre Miller</h2>
+                <h2 className="lg:text-4xl start ml-32 flex items-center rochester">Diandre Miller</h2>
                 <div className="ml-auto flex items-center space-x-4 mr-10">
                 <Link to='https://www.linkedin.com/in/diandre-miller/' target="blank"><img className="h-11" style={{ transform: 'translateX(-128px)'}} src={linkedIn} /></Link>
                 <Link to='https://github.com/DiandreMiller' target="blank"><img className="h-10" style={{ transform: 'translateX(-128px)'}} src={gitHub} /></Link>
