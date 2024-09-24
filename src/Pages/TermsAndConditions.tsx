3aa59c8b8e306b4049ecb9b0add4be53abbc43b4
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-24 01:02:01 -0400

    pages: added terms and conditions page.

---
+++ src/Pages/TermsAndConditions.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TermsAndConditions = () => {
+
+    return (
+
+        <div>
+            <h2>Terms And Conditions</h2>
+        </div>
+    )
+}
+
+export default TermsAndConditions;
