8a9473406148f8f33be2a5b5890ae162b5a5a27e
Diandre Miller <diandremiller@Diandres-MacBook-Air.local>
2024-09-26 16:04:58 -0400

    design: updated typing motion, still a bit buggy

--- src/Components/BasketballCourt.tsx
+++ src/Components/BasketballCourt.tsx
@@ -7,58 +7,73 @@ import cloud from '../Assets/cloud1.png'
 import banner from '../Assets/banner.png'
 import Instructions from "./Instructions";
 import TypeWriter from "./Typewriter";
+import { v4 as uuidv4 } from 'uuid';
 
 interface HomeProps {
     instructions: string[];
 }
 
-const BasketballCourt: React.FC<HomeProps> = ({instructions}) => {
-
+const BasketballCourt: React.FC<HomeProps> = ({ instructions }) => {
     const [isButtonVisible, setIsButtonVisible] = useState<boolean>(true);
     const [isBannerVisible, setIsBannerVisible] = useState<boolean>(true);
     const [isNameVisible, setIsNameVisible] = useState<boolean>(true);
-    const [isInstructionsVisibible, setIsInstructionsVisible] = useState<string[]>(['']);
+    const [currentInstruction, setCurrentInstruction] = useState<string>(instructions[0]);
+    const [visibleInstructions, setVisibleInstructions] = useState<string[]>([]);
     const [isInstructionButtonVisible, setIsInstructionButtonVisible] = useState<boolean>(false);
+    const [instructionIndex, setInstructionIndex] = useState<number>(0);
 
     const handleGetStarted = (): void => {
         setIsBannerVisible(false);
         setIsButtonVisible(false);
         setIsNameVisible(false);
-        setIsInstructionsVisible(instructions);
-        setIsInstructionButtonVisible(true);
-        console.log('instructions:', instructions);
+        setVisibleInstructions([instructions[0]]); 
+        setCurrentInstruction(instructions[0]);
+        setIsInstructionButtonVisible(true); 
     }
 
-    
+    const handleContinue = (): void => {
+        if (instructionIndex + 1 < instructions.length) {
+            const nextIndex = instructionIndex + 1;
+            setInstructionIndex(nextIndex);
+            setCurrentInstruction(instructions[nextIndex]);
+            setVisibleInstructions((prevInstructions) => [
+                ...prevInstructions,
+                instructions[nextIndex],
+            ]); 
+        } else {
+            setIsInstructionButtonVisible(false);
+        }
+    }
 
     return (
-        <div className="w-screen h-screen bg-cover bg-center transform -translate-x-28 p-0 overflow-x-hidden" style={{ backgroundImage: `url(${court})`}}>
+        <div className="w-screen h-screen bg-cover bg-center transform -translate-x-28 p-0 overflow-x-hidden" style={{ backgroundImage: `url(${court})` }}>
             <BallBounce />
             {isBannerVisible && (
                 <div className="flex justify-center">
-                    <img className='z-20' src={banner} style={{ transform: 'translateY(-300px)', width: '600px'}} alt="banner" />
+                    <img className='z-20' src={banner} style={{ transform: 'translateY(-300px)', width: '600px' }} alt="banner" />
                 </div>
             )}
 
-            {isInstructionsVisibible && (
+            {visibleInstructions.length > 0 && (
                 <div>
                     <h3>
-                        {isInstructionsVisibible.map((instruction) => (
-                            <TypeWriter  text={instruction} />
+                        {visibleInstructions.map((instruction) => (
+                            <TypeWriter key={uuidv4()} instructions={instruction} />
                         ))}
                     </h3>
                 </div>
             )}
 
-            <img className='h-36 cloud z-10' style={{ transform: 'translateY(-380px)  rotate(10deg)' }} src={cloud} alt="cloud"/>
-            <img className='h-36 cloud2 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud"/>
-            <img className='h-36 cloud3 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud"/>
-            <img className='h-36 cloud4 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud"/>
-            <img className='h-36 cloud5 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud"/>
-            <img className='h-36 cloud6 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud"/>
-            <div className="flex justify-center item-center" style={{ transform: 'translateY(-180px)'}}>
+            <img className='h-36 cloud z-10' style={{ transform: 'translateY(-380px)  rotate(10deg)' }} src={cloud} alt="cloud" />
+            <img className='h-36 cloud2 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud" />
+            <img className='h-36 cloud3 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud" />
+            <img className='h-36 cloud4 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud" />
+            <img className='h-36 cloud5 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud" />
+            <img className='h-36 cloud6 z-10' style={{ transform: 'translateY(-380px) rotate(10deg)' }} src={cloud} alt="cloud" />
+
+            <div className="flex justify-center item-center" style={{ transform: 'translateY(-180px)' }}>
                 {isNameVisible && (
-                    <h2 className="rochester lg:text-9xl" style={{color: 'rgb(255, 0, 0)'}}>Diandre Miller</h2>
+                    <h2 className="rochester lg:text-9xl" style={{ color: 'rgb(255, 0, 0)' }}>Diandre Miller</h2>
                 )}
             </div>
 
@@ -67,12 +82,10 @@ const BasketballCourt: React.FC<HomeProps> = ({instructions}) => {
             )}
 
             {isInstructionButtonVisible && (
-                <button className="button animate-bounce" onClick={handleGetStarted}> Continue</button>
+                <button className="button animate-bounce" onClick={handleContinue}> Continue</button>
             )}
-            
         </div>
-
-    )
+    );
 }
 
 export default BasketballCourt;
--- src/Components/Typewriter.tsx
+++ src/Components/Typewriter.tsx
@@ -1,35 +1,48 @@
-import React, { useState, useEffect } from "react";
+import Typed from 'react-typed';
 
-interface TypeWriterProp {
-    text: string;
-    delay: number;
-    
-}
-
-const TypeWriter: React.FC<TypeWriterProp> = ({text, delay}) => {
-
-    const [currentText, setCurrentText] = useState<string>('');
-    const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-    useEffect(() => {
-        if(currentIndex < text.length) {
-            const timeOut = setTimeout(() => {
-                setCurrentText(previousText => previousText + text[currentIndex]);
-                setCurrentIndex(previousIndex => previousIndex + 1);
-            }, delay);
-
-            return () => clearTimeout(timeOut);
-        }
-    }, [currentIndex, delay, text])
-
-
-    
+const TypeWriter = ({instructions}: {instructions: string}) => {
     return (
-        <span>
-            {currentText}
-        </span>
-
-    )
+        <Typed
+            strings={[instructions]}
+            typeSpeed={40}
+        />
+    );
 }
 
 export default TypeWriter;
+
+// import React, { useState, useEffect } from "react";
+
+// interface TypeWriterProp {
+//     text: string;
+//     delay: number;
+    
+// }
+
+// const TypeWriter: React.FC<TypeWriterProp> = ({text, delay}) => {
+
+//     const [currentText, setCurrentText] = useState<string>('');
+//     const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+//     useEffect(() => {
+//         if(currentIndex < text.length) {
+//             const timeOut = setTimeout(() => {
+//                 setCurrentText(previousText => previousText + text[currentIndex]);
+//                 setCurrentIndex(previousIndex => previousIndex + 1);
+//             }, delay);
+
+//             return () => clearTimeout(timeOut);
+//         }
+//     }, [currentIndex, delay, text])
+
+
+    
+//     return (
+//         <span>
+//             {currentText}
+//         </span>
+
+//     )
+// }
+
+// export default TypeWriter;
