4f80f752fbb8ad2b54b33160b6337c8fa339eb8f
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-09-26 19:37:42 -0400

    functionality: updated typing speed

--- src/Components/Typewriter.tsx
+++ src/Components/Typewriter.tsx
@@ -1,48 +1,24 @@
-import Typed from 'react-typed';
+import React, { useState, useEffect } from 'react';
+import { ReactTyped } from 'react-typed';
+import { v4 as uuidv4 } from 'uuid';
 
 const TypeWriter = ({instructions}: {instructions: string}) => {
-    return (
-        <Typed
-            strings={[instructions]}
-            typeSpeed={40}
-        />
-    );
+
+    const [currentText, setCurrentText] = useState<string>('');
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+    useEffect(() => {
+        if (currentIndex < instructions.length) {
+            const timer = setTimeout(() => {
+                setCurrentText(prev => prev + instructions[currentIndex]);
+                setCurrentIndex(prev => prev + 1);
+            }, 50);
+            return () => clearTimeout(timer);
+        }
+    }, [currentIndex, instructions]);
+
+    return <div className='text-black'>{currentText}</div>;
 }
 
 export default TypeWriter;
 
-// import React, { useState, useEffect } from "react";
-
-// interface TypeWriterProp {
-//     text: string;
-//     delay: number;
-    
-// }
-
-// const TypeWriter: React.FC<TypeWriterProp> = ({text, delay}) => {
-
-//     const [currentText, setCurrentText] = useState<string>('');
-//     const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-//     useEffect(() => {
-//         if(currentIndex < text.length) {
-//             const timeOut = setTimeout(() => {
-//                 setCurrentText(previousText => previousText + text[currentIndex]);
-//                 setCurrentIndex(previousIndex => previousIndex + 1);
-//             }, delay);
-
-//             return () => clearTimeout(timeOut);
-//         }
-//     }, [currentIndex, delay, text])
-
-
-    
-//     return (
-//         <span>
-//             {currentText}
-//         </span>
-
-//     )
-// }
-
-// export default TypeWriter;
