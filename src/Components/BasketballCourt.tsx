e7f29e6cc80790ea6249ca10a29427a93fedfd45
Diandre Miller <diandremiller@Diandres-MacBook-Air.local>
2024-09-26 12:35:31 -0400

    functionality: added button after the click of the first button

--- src/Components/BasketballCourt.tsx
+++ src/Components/BasketballCourt.tsx
@@ -5,22 +5,31 @@ import court from '../Assets/cartoon-basketball-court-vector.jpg'
 import BallBounce from "./BallBounce";
 import cloud from '../Assets/cloud1.png'
 import banner from '../Assets/banner.png'
+import Instructions from "./Instructions";
+import TypeWriter from "./Typewriter";
 
 interface HomeProps {
     instructions: string[];
 }
 
-const BasketballCourt: React.FC<HomeProps> = () => {
+const BasketballCourt: React.FC<HomeProps> = ({instructions}) => {
 
     const [isButtonVisible, setIsButtonVisible] = useState<boolean>(true);
     const [isBannerVisible, setIsBannerVisible] = useState<boolean>(true);
     const [isNameVisible, setIsNameVisible] = useState<boolean>(true);
+    const [isInstructionsVisibible, setIsInstructionsVisible] = useState<string[]>(['']);
+    const [isInstructionButtonVisible, setIsInstructionButtonVisible] = useState<boolean>(false);
 
     const handleGetStarted = (): void => {
         setIsBannerVisible(false);
         setIsButtonVisible(false);
         setIsNameVisible(false);
+        setIsInstructionsVisible(instructions);
+        setIsInstructionButtonVisible(true);
+        console.log('instructions:', instructions);
     }
+
+    
 
     return (
         <div className="w-screen h-screen bg-cover bg-center transform -translate-x-28 p-0 overflow-x-hidden" style={{ backgroundImage: `url(${court})`}}>
@@ -28,6 +37,16 @@ const BasketballCourt: React.FC<HomeProps> = () => {
             {isBannerVisible && (
                 <div className="flex justify-center">
                     <img className='z-20' src={banner} style={{ transform: 'translateY(-300px)', width: '600px'}} alt="banner" />
+                </div>
+            )}
+
+            {isInstructionsVisibible && (
+                <div>
+                    <h3>
+                        {isInstructionsVisibible.map((instruction) => (
+                            <TypeWriter  text={instruction} />
+                        ))}
+                    </h3>
                 </div>
             )}
 
@@ -45,6 +64,10 @@ const BasketballCourt: React.FC<HomeProps> = () => {
 
             {isButtonVisible && (
                 <button className="button animate-bounce" onClick={handleGetStarted}> Click Here To Get Started!</button>
+            )}
+
+            {isInstructionButtonVisible && (
+                <button className="button animate-bounce" onClick={handleGetStarted}> Continue</button>
             )}
             
         </div>
