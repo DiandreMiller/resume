f2c267261da3c35c6c26283fd263334c6dcca056
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-09-01 20:15:28 -0400

    update

--- src/Pages/MeetMe.tsx
+++ src/Pages/MeetMe.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import linkedInLogo from '../Assets/linkedInLogo.png'
 import githubLogo from '../Assets/github-Logo.png'
